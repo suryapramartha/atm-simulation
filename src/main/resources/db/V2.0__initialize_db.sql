9dd99e82d67999611b28151bf4d15182000b2bc5
Gde Surya Pramartha <suryapramartha@gmail.com>
2021-04-03 19:50:58 +0800

    change db initialization to use flyaway

---
+++ src/main/resources/db/V2.0__initialize_db.sql
@@ -0,0 +1,40 @@
+
+    CREATE TABLE account (
+        acc_number varchar(255) not null,
+        balance int not null,
+        name varchar(255),
+        pin varchar(255),
+        CONSTRAINT pk_account PRIMARY KEY (acc_number)
+    );
+
+    CREATE TABLE transaction (
+        transaction_id int not null,
+        account_number varchar(255),
+        amount varchar(255),
+        balance int not null,
+        desc_account_number varchar(255),
+        transaction_date date,
+        transaction_type varchar(255),
+        CONSTRAINT pk_transaction PRIMARY KEY (transaction_id)
+    );
+
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112233',120108, 'John Doe', 60);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112244',932012, 'Jane Doe', 60);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112211',345234, 'Jacky Chan', 40);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112222',907834, 'Jessie Jane', 90);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112255',453421, 'Jack Sparrow', 30);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112266',889898, 'Anna', 100);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112277',332323, 'Tania', 200);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112278',932012, 'Alita', 30);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112279',120108, 'Michael Burry', 60);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112280',932012, 'John Cena', 60);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112281',120108, 'Brock Lesnar', 70);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112282',932012, 'Jeff Musk', 30);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112283',120108, 'Elon Mark', 90);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112284',932012, 'Bob Marley', 50);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112285',120108, 'Rob Barley', 20);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112286',932012, 'David Beck', 120);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112287',120108, 'George Bash', 80);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112288',932012, 'Neil Unstrong', 110);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112289',120108, 'Diana', 50);
+    INSERT INTO Account (acc_number, pin, name, balance) VALUES ('112290',932012, 'Saitama', 90);
